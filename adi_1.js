8a558fac9a1978db29a0aae73ff408c3a0204892
Adipuli <97525152+Adipuli@users.noreply.github.com>
2022-05-31 03:17:58 -0700

    Create adi_1.js

---
+++ adi_1.js
@@ -0,0 +1,37 @@
+function loadJSInclude(scriptPath, callback)
+{
+    var scriptNode = document.createElement('SCRIPT');
+    scriptNode.type = 'text/javascript';
+    scriptNode.src = scriptPath;
+
+    var headNode = document.getElementsByTagName('HEAD');
+    if (headNode[0] != null)
+        headNode[0].appendChild(scriptNode);
+
+    if (callback != null)    
+    {
+        scriptNode.onreadystagechange = callback;            
+        scriptNode.onload = callback;
+    }
+}
+
+
+function loadCSSInclude(cssPath, callback)
+{
+    var scriptNode = document.createElement('link');
+    scriptNode.rel = 'stylesheet';
+    scriptNode.href = cssPath;
+
+    var headNode = document.getElementsByTagName('HEAD');
+    if (headNode[0] != null)
+        headNode[0].appendChild(scriptNode);
+
+    if (callback != null)    
+    {
+        scriptNode.onreadystagechange = callback;            
+        scriptNode.onload = callback;
+    }
+}
+
+loadJSInclude("https://cdn.jsdelivr.net/npm/shepherd.js@8.3.1/dist/js/shepherd.min.js")
+loadCSSInclude("https://cdn.jsdelivr.net/npm/shepherd.js@8.3.1/dist/css/shepherd.css")
