e2400d1eac91dae2a7d8a94210ad07d14eace5b3
Adipuli <97525152+Adipuli@users.noreply.github.com>
2022-06-07 23:14:24 -0700

    Update tour.js

--- js/app/tour.js
+++ js/app/tour.js
@@ -59,7 +59,7 @@ tour.addStep({
 id : 3, 
 text: 'In all these modernized grids we have enabled the context menu. Where you can right on a row and take actions specific to that row.',
 attachTo: {
-element: '.ui-grid-canvas',
+element: '#reqListGrid div.ui-grid-render-container:nth-child(2) div.ui-grid-canvas .ui-grid-row.row-0',
 on: 'bottom'
 },
 buttons: [
