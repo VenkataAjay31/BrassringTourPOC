e16ebbd8fe0b536686211b97f54e34e83101f56e
Venkata Ajay Mamidipaka <98172062+VenkataAjay31@users.noreply.github.com>
2022-06-09 20:38:31 +0530

    Update inject.js

--- inject.js
+++ inject.js
@@ -37,4 +37,4 @@ loadJSInclude("https://venkataajay31.github.io/BrassringTourPOC/js/lib/shepherd.
 loadJSInclude("https://venkataajay31.github.io/BrassringTourPOC/js/app/tour.js")
 
 loadCSSInclude("https://venkataajay31.github.io/BrassringTourPOC/css/lib/shepherd.css")
-loadCSSInclude("https://venkataajay31.github.io/BrassringTourPOC/css/app/custom.css")
+loadCSSInclude("https://venkataajay31.github.io/BrassringTourPOC/css/app/th_grey.css")
