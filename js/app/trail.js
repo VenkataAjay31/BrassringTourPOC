162edba4525a62f61a5c222fca768bd55e74dfab
sriharshavarada <36196732+sriharshavarada@users.noreply.github.com>
2022-07-12 21:48:08 +0530

    Update trail.js

--- js/app/trail.js
+++ js/app/trail.js
@@ -1,4 +1,4 @@
-$(".welcomeContentTabContainer").append("<div class=\"GuidedTour\" onclick=\"triggerTour();\">Guided Tour</div>")
+$(".mainHeader .right").append("<div class=\"GuidedTour\" onclick=\"triggerTour();\">Guided Tour</div>")
 
 function triggerTour()
 {
